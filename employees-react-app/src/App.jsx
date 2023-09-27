f83466fdc19ce057a24b8fd4210124aa2197777e
Nikolay Cherepov <aeconick@gmail.com>
2023-09-27 09:24:03 +0300

    Add moment to project

--- employees-react-app/src/App.jsx
+++ employees-react-app/src/App.jsx
@@ -1,19 +1,28 @@
 import { useState } from "react";
 import Papa from "papaparse";
+import moment from "moment";
 import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
 
-
   //parse CSV file & store it in the component state
   const handleFileUpload = (e) => {
     const file = e.target.files[0]; //file list objects that contails info about the selected files
     Papa.parse(file, {
-      header: false,
+      header: true,
       complete: (results) => {
         setData(results.data);
+        console.log(results.data);
+        processCommonProjects(results.data);
       },
+    });
+  };
+
+  // Process data to find common projects and calculate days worked
+  const processCommonProjects = (data) => {
+    data.forEach((row1, index1) => {
+     console.log(moment(row1.DateFrom));
     });
   };
 
@@ -21,7 +30,6 @@ function App() {
     <div className="main-container">
       <p>select csv file:</p>
       <input type="file" accept=".csv" onChange={handleFileUpload} />
-      <p>{data}</p>
     </div>
   );
 }
