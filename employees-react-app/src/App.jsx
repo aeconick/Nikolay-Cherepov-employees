8253b0332bb276f370b7f91aa728b4848f5e957e
Nikolay Cherepov <aeconick@gmail.com>
2023-09-27 18:42:58 +0300

    Get all pairs of employees working on same projects

--- employees-react-app/src/App.jsx
+++ employees-react-app/src/App.jsx
@@ -5,15 +5,17 @@ import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
+  const [result, setResult] = useState([]);
 
   //parse CSV file & store it in the component state
   const handleFileUpload = (e) => {
     const file = e.target.files[0]; //file list objects that contails info about the selected files
     Papa.parse(file, {
-      header: true,
+      header: false,
+      skipEmptyLines: true,
+      dynamicTyping: true, //TODO: maybe wont work
       complete: (results) => {
         setData(results.data);
-        console.log(results.data);
         processCommonProjects(results.data);
       },
     });
@@ -22,7 +24,12 @@ function App() {
   // Process data to find common projects and calculate days worked
   const processCommonProjects = (data) => {
     data.forEach((row1, index1) => {
-     console.log(moment(row1.DateFrom));
+      data.forEach((row2, index2) => {
+        //row[EmpID, ProjectID, DateFrom, DateTo]
+        if (index1 !== index2 && row1[0] < row2[0] && row1[1] === row2[1]) {
+          console.log(row1, row2);
+        }
+      });
     });
   };
 
