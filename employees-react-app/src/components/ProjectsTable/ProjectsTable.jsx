8bb3bb84137f77a6c430e7ee9b8ae2cc7a696d19
Nikolay Cherepov <aeconick@gmail.com>
2023-09-28 11:55:36 +0300

    Update styles

--- employees-react-app/src/components/ProjectsTable/ProjectsTable.jsx
+++ employees-react-app/src/components/ProjectsTable/ProjectsTable.jsx
@@ -1,27 +1,26 @@
 import "./ProjectsTable.css";
 
-export const ProjectsTable = ({mostWorkedPair}) => {
-
+export const ProjectsTable = ({ mostWorkedPair }) => {
   return (
     <table className="common-projects-table">
-          <thead>
-            <tr>
-              <th>Employee ID #1</th>
-              <th>Employee ID #2</th>
-              <th>Project ID</th>
-              <th>Days worked</th>
-            </tr>
-          </thead>
-          <tbody>
-            {mostWorkedPair.commonProjects.map((project, index) => (
-              <tr key={index}>
-                <td>{mostWorkedPair.EmpID1}</td>
-                <td>{mostWorkedPair.EmpID2}</td>
-                <td>{project.ProjectID}</td>
-                <td>{project.daysWorked}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <thead>
+        <tr>
+          <th>Employee ID #1</th>
+          <th>Employee ID #2</th>
+          <th>Project ID</th>
+          <th>Days worked</th>
+        </tr>
+      </thead>
+      <tbody>
+        {mostWorkedPair.commonProjects.map((project, index) => (
+          <tr key={index}>
+            <td>{mostWorkedPair.EmpID1}</td>
+            <td>{mostWorkedPair.EmpID2}</td>
+            <td>{project.ProjectID}</td>
+            <td>{project.daysWorked}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
